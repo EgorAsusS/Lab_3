5fe3039628568a506a91dde5b54fe3694b7612ad
EgorAsusS <egor.firsov.egor@yandex.ru>
2026-04-13 21:12:46 +0700

    Rename functions and simple fixes
    Add function printLongBv
    shifts not do to the end (from the beginning) of the long bv

--- Lab_3/main.c
+++ Lab_3/main.c
@@ -2,31 +2,32 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-unsigned char* convert_str_to_long_bv(char* str, int* len);
-char* convert_long_bv_to_str(unsigned char* vec, size_t len);
-unsigned char* log_mul(unsigned char* vec1, size_t len1, unsigned char* vec2, size_t len2);
-unsigned char* log_add(unsigned char* vec1, size_t len1, unsigned char* vec2, size_t len2);
-unsigned char* xor (unsigned char* vec1, size_t len1, unsigned char* vec2, size_t len2);
-void invert_bv(unsigned char* vec, size_t len);
-void shift_k_left(unsigned char* vec, size_t len, size_t k);
-void shift_k_right(unsigned char* vec, size_t len, size_t k);
-void set_k(unsigned char* vec, size_t len, size_t k);
-void reset_k(unsigned char* vec, size_t len, size_t k);
+unsigned char* convertStrToLongBv(char* str, int* bits);
+char* convertLongBvToStr(unsigned char* vec, size_t bits);
+void printLongBv(unsigned char* vec, size_t bits);
+unsigned char* logMul(unsigned char* vecA, size_t bitsA, unsigned char* vecB, size_t bitsB);
+unsigned char* logSum(unsigned char* vecA, size_t bitsA, unsigned char* vecB, size_t bitsB);
+unsigned char* sumMod2 (unsigned char* vecA, size_t bitsA, unsigned char* vecB, size_t bitsB);
+void inversion(unsigned char* vec, size_t bits);
+void shiftLeft(unsigned char* vec, size_t bits, size_t k);
+void shiftRight(unsigned char* vec, size_t bits, size_t k);
+void set1(unsigned char* vec, size_t bits, size_t k);
+void set0(unsigned char* vec, size_t bits, size_t k);
 
 int main() {
     // & | ^
     //char text1[] = "111";
     //char text2[] = "111";
-    //int len1 = 0;
-    //int len2 = 0;
-    //unsigned char* vec1 = convert_str_to_long_bv(text1, &len1);
-    //unsigned char* vec2 = convert_str_to_long_bv(text2, &len2);
-    //unsigned char* vec = xor(vec1, len1, vec2, len2);
-    //int len = len1;
+    //int bitsA = 0;
+    //int bitsB = 0;
+    //unsigned char* vecA = convertStrToLongBv(text1, &bitsA);
+    //unsigned char* vecB = convertStrToLongBv(text2, &bitsB);
+    //unsigned char* vec = sumMod2(vecA, bitsA, vecB, bitsB);
+    //int bits = bitsA;
     //if (vec) {
-    //    char* vec1_str = convert_long_bv_to_str(vec1, len1);
-    //    char* vec2_str = convert_long_bv_to_str(vec2, len2);
-    //    char* vec_str = convert_long_bv_to_str(vec, len);
+    //    char* vec1_str = convertLongBvToStr(vecA, bitsA);
+    //    char* vec2_str = convertLongBvToStr(vecB, bitsB);
+    //    char* vec_str = convertLongBvToStr(vec, bits);
     //    printf("%s\n", vec1_str);
     //    printf("%s\n", vec2_str);
     //    printf("%s\n", vec_str);
@@ -38,16 +39,16 @@ int main() {
     //    printf("Error\n");
     //}
     //free(vec);
-    //free(vec1);
-    //free(vec2);
+    //free(vecA);
+    //free(vecB);
 
 
     // ~
     //char text[] = "111";
     //int leni = 0;
-    //unsigned char* veci = convert_str_to_long_bv(text, &leni);
-    //invert_bv(veci, leni);
-    //char* veci_str = convert_long_bv_to_str(veci, leni);
+    //unsigned char* veci = convertStrToLongBv(text, &leni);
+    //inversion(veci, leni);
+    //char* veci_str = convertLongBvToStr(veci, leni);
     //printf("%s\n", veci_str);
     //free(veci_str);
     //free(veci);
@@ -55,51 +56,51 @@ int main() {
 
     // <<
     //char text[] = "11111111111111111111111111111111111111111";
-    //int len = 0;
-    //unsigned char* vec = convert_str_to_long_bv(text, &len);
-    //shift_k_left(vec, len, 40);
-    //char* vec_str = convert_long_bv_to_str(vec, len);
+    //int bits = 0;
+    //unsigned char* vec = convertStrToLongBv(text, &bits);
+    //shiftLeft(vec, bits, 40);
+    //char* vec_str = convertLongBvToStr(vec, bits);
     //printf("%s\n", vec_str);
     //free(vec_str);
     //free(vec);
 
     // >>
-    //char text[] = "000000000";
-    //int len = 0;
-    //unsigned char* vec = convert_str_to_long_bv(text, &len);
-    //invert_bv(vec, len);
-    //shift_k_left(vec, len, 1);
-    //char* vec_str = convert_long_bv_to_str(vec, len);
+    //char text[] = "00000000000000000";
+    //int bits = 0;
+    //unsigned char* vec = convertStrToLongBv(text, &bits);
+    //inversion(vec, bits);
+    //shiftRight(vec, bits, 9);
+    //char* vec_str = convertLongBvToStr(vec, bits);
     //printf("%s\n", vec_str);
     //free(vec_str);
     //free(vec);
 
-    char text[] = "1111111111";
-    int len = 0;
-    unsigned char* vec = convert_str_to_long_bv(text, &len);
-    reset_k(vec, len, 0);
-    char* vec_str = convert_long_bv_to_str(vec, len);
-    printf("%s\n", vec_str);
-    free(vec_str);
-    free(vec);
+    // char text[] = "1111111111";
+    // int bits = 0;
+    // unsigned char* vec = convertStrToLongBv(text, &bits);
+    // set0(vec, bits, 1);
+    // char* vec_str = convertLongBvToStr(vec, bits);
+    // printf("%s\n", vec_str);
+    // free(vec_str);
+    // free(vec);
 
 
     return 0;
 }
 
 
-unsigned char* convert_str_to_long_bv(char* str, int* len) {
-    int cells = 0, ix = 0;
+unsigned char* convertStrToLongBv(char* str, int* bits) {
+    int bytes = 0, ix = 0;
     unsigned char mask = 1;
     unsigned char* vec = NULL;
-    if (str && len) {
-        *len = strlen(str);
-        cells = ((*len - 1) / 8) + 1;
-        vec = (unsigned char*)calloc(cells, sizeof(unsigned char));
+    if (str && bits) {
+        *bits = strlen(str);
+        bytes = ((*bits - 1) / 8) + 1;
+        vec = (unsigned char*)calloc(bytes, sizeof(unsigned char));
         if (vec) {
-            for (int i = 0; i < cells; i++) {
+            for (int i = 0; i < bytes; i++) {
                 mask = 1;
-                for (int j = 0; j < 8 && (ix < *len); j++) {
+                for (int j = 0; j < 8 && (ix < *bits); j++) {
                     if (str[ix] != '0') {
                         vec[i] = vec[i] | mask;
                     }
@@ -112,16 +113,16 @@ unsigned char* convert_str_to_long_bv(char* str, int* len) {
     return vec;
 }
 
-char* convert_long_bv_to_str(unsigned char* vec, size_t len) {
+char* convertLongBvToStr(unsigned char* vec, size_t bits) {
     int ix = 0;
     unsigned char mask = 1;
     char* str = NULL;
-    if (vec && len) {
-       str = (char*)calloc(len + 1, sizeof(char));
+    if (vec && bits) {
+        str = (char*)calloc(bits + 1, sizeof(char));
         if (str) {
-            for (int i = 0; i < len; i++) {
+            for (int i = 0; i < bits; i++) {
                 mask = 1;
-                for (int j = 0; j < 8 && (ix < len); j++) {
+                for (int j = 0; j < 8 && (ix < bits); j++) {
                     if (vec[i] & mask) {
                         str[ix] = '1';
                     }
@@ -138,74 +139,86 @@ char* convert_long_bv_to_str(unsigned char* vec, size_t len) {
     return str;
 }
 
-unsigned char* log_mul(unsigned char* vec1, size_t len1, unsigned char* vec2, size_t len2) {
+
+void printLongBv(unsigned char* vec, size_t bits) {
+    if (vec && bits) {
+        char* vec_str = convertLongBvToStr(vec, bits);
+        if (vec_str) {
+            printf("%s\n", vec_str);
+        }
+    }
+}
+
+unsigned char* logMul(unsigned char* vecA, size_t bitsA, unsigned char* vecB, size_t bitsB) {
     unsigned char* vec = NULL;
-    if (vec1 && vec2 && len1 && len2 && len1 == len2) {
-        int cells = ((len1 - 1) / 8) + 1;
-        vec = (char*)calloc(len1, sizeof(unsigned char));
+    if (vecA && vecB && bitsA && bitsB && bitsA == bitsB) {
+        int bytes = ((bitsA - 1) / 8) + 1;
+        vec = (char*)calloc(bitsA, sizeof(unsigned char));
         if (vec) {
-            for (int i = 0; i < cells; i++) {
-                vec[i] = vec1[i] & vec2[i];
+            for (int i = 0; i < bytes; i++) {
+                vec[i] = vecA[i] & vecB[i];
             }
         }
     }
     return vec;
 }
 
-unsigned char* log_add(unsigned char* vec1, size_t len1, unsigned char* vec2, size_t len2) {
+unsigned char* logSum(unsigned char* vecA, size_t bitsA, unsigned char* vecB, size_t bitsB) {
     unsigned char* vec = NULL;
-    if (vec1 && vec2 && len1 && len2 && len1 == len2) {
-        int cells = ((len1 - 1) / 8) + 1;
-        vec = (char*)calloc(len1, sizeof(unsigned char));
+    if (vecA && vecB && bitsA && bitsB && bitsA == bitsB) {
+        int bytes = ((bitsA - 1) / 8) + 1;
+        vec = (char*)calloc(bitsA, sizeof(unsigned char));
         if (vec) {
-            for (int i = 0; i < cells; i++) {
-                vec[i] = vec1[i] | vec2[i];
+            for (int i = 0; i < bytes; i++) {
+                vec[i] = vecA[i] | vecB[i];
             }
         }
     }
     return vec;
 }
 
-unsigned char* xor(unsigned char* vec1, size_t len1, unsigned char* vec2, size_t len2) {
+unsigned char* sumMod2 (unsigned char* vecA, size_t bitsA, unsigned char* vecB, size_t bitsB) {
     unsigned char* vec = NULL;
-    if (vec1 && vec2 && len1 && len2 && len1 == len2) {
-        int cells = ((len1 - 1) / 8) + 1;
-        vec = (char*)calloc(len1, sizeof(unsigned char));
+    if (vecA && vecB && bitsA && bitsB && bitsA == bitsB) {
+        int bytes = ((bitsA - 1) / 8) + 1;
+        vec = (char*)calloc(bitsA, sizeof(unsigned char));
         if (vec) {
-            for (int i = 0; i < cells; i++) {
-                vec[i] = vec1[i] ^ vec2[i];
+            for (int i = 0; i < bytes; i++) {
+                vec[i] = vecA[i] ^ vecB[i];
             }
         }
     }
     return vec;
 }
 
-void invert_bv(unsigned char* vec, size_t len) {
-    if (vec && len) {
-        int cells = ((len - 1) / 8) + 1;
-        for (int i = 0; i < cells; i++) {
+void inversion(unsigned char* vec, size_t bits) {
+    if (vec && bits) {
+        int bytes = ((bits - 1) / 8) + 1;
+        for (int i = 0; i < bytes; i++) {
             vec[i] = ~vec[i];
         }
-        int tail = cells * 8 - len;
+        int tail = bytes * 8 - bits;
         unsigned char mask = 0;
         mask = -1;
         mask = mask >> tail;
-        vec[cells - 1] = vec[cells - 1] & mask;
+        vec[bytes - 1] = vec[bytes - 1] & mask;
     }
 }
 
-void shift_k_left(unsigned char* vec, size_t len, size_t k) {
-    if (vec && len && k <= len) {
-        int cells = ((len - 1) / 8) + 1;
+void shiftLeft(unsigned char* vec, size_t bits, size_t k) {
+    if (vec && bits && k <= bits) {
+        int bytes = ((bits - 1) / 8) + 1;
         size_t j = k / 8;
         size_t i = 0;
+        size_t i_i = 0;
         if (j) {
-            while (j < cells) {
+            while (j < bytes) {
                 vec[i] = vec[j];
                 i++;
                 j++;
             }
-            while (i < cells) {
+            i_i = i - 1;
+            while (i < bytes) {
                 vec[i] = 0;
                 i++;
             }
@@ -214,7 +227,7 @@ void shift_k_left(unsigned char* vec, size_t len, size_t k) {
         unsigned char ib = 0;
         unsigned char mask = -1;
         int ix = 0;
-        for (ix = 0; ix < cells - 1; ix++) {
+        for (ix = 0; ix < i_i; ix++) {
             mask = -1;
             vec[ix] = vec[ix] >> bit;
             mask = mask >> (8 - bit);
@@ -223,24 +236,26 @@ void shift_k_left(unsigned char* vec, size_t len, size_t k) {
             vec[ix] = vec[ix] | ib;
         }
         vec[ix] = vec[ix] >> bit;
-        int tail = cells * 8 - len;
+        int tail = bytes * 8 - bits;
         mask = -1;
         mask = mask >> tail;
-        vec[cells - 1] = vec[cells - 1] & mask;
+        vec[bytes - 1] = vec[bytes - 1] & mask;
     }
 }
 
-void shift_k_right(unsigned char* vec, size_t len, size_t k) {
-    if (vec && len && k <= len) {
-        int cells = ((len - 1) / 8) + 1;
+void shiftRight(unsigned char* vec, size_t bits, size_t k) {
+    if (vec && bits && k <= bits) {
+        int bytes = ((bits - 1) / 8) + 1;
         size_t j = k / 8;
         size_t i = 0;
+        size_t i_i = 0;
         if (j) {
-            while (j < cells) {
+            while (j < bytes) {
                 vec[j] = vec[i];
                 i++;
                 j++;
             }
+            i_i = i - 1;
             i = 0;
             while (i < k / 8) {
                 vec[i] = 0;
@@ -252,7 +267,7 @@ void shift_k_right(unsigned char* vec, size_t len, size_t k) {
         unsigned char iby = 0;
         unsigned char mask = -1;
         int ix = 0;
-        for (ix = 0; ix < cells; ix++) {
+        for (ix = i_i; ix < bytes; ix++) {
             mask = -1;
             mask = mask << (8 - bit);
             iby = vec[ix] & mask;
@@ -261,15 +276,15 @@ void shift_k_right(unsigned char* vec, size_t len, size_t k) {
             vec[ix] = vec[ix] | ibx;
             ibx = iby;
         }
-        int tail = cells * 8 - len;
+        int tail = bytes * 8 - bits;
         mask = -1;
         mask = mask >> tail;
-        vec[cells - 1] = vec[cells - 1] & mask;
+        vec[bytes - 1] = vec[bytes - 1] & mask;
     }
 }
 
-void set_k(unsigned char* vec, size_t len, size_t k) {
-    if (vec && len && k <= len) {
+void set1(unsigned char* vec, size_t bits, size_t k) {
+    if (vec && bits && k <= bits) {
         unsigned char mask = 1;
         size_t byte = k / 8;
         size_t bit = k % 8;
@@ -278,8 +293,8 @@ void set_k(unsigned char* vec, size_t len, size_t k) {
     }
 }
 
-void reset_k(unsigned char* vec, size_t len, size_t k) {
-    if (vec && len && k <= len) {
+void set0(unsigned char* vec, size_t bits, size_t k) {
+    if (vec && bits && k <= bits) {
         unsigned char mask = 1;
         size_t byte = k / 8;
         size_t bit = k % 8;
